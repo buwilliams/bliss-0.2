53d0f5f28c1523b7b12c946d49ac207f30336cab
Buddy Williams <buddywilliams@gmail.com>
2017-06-01 18:00:03 -0400

    added forever support

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ echo 'Updating node deps...'
 yarn install
 
 echo 'Building Bliss...'
-node_module/.bin/jake build
+node_modules/.bin/jake build
 
 echo 'Starting express web server...'
-forever start -l logs/forever.log -o logs/bliss-out.log -e logs/bliss-err.log node_modules/.bin/jake server
+forever start -p . -l ./logs/forever.log -o ./logs/bliss-out.log -e ./logs/bliss-err.log node_modules/.bin/jake server
