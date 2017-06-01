10bc3b809caeefcdd16764042b6c24ea04fbfa3c
Buddy Williams <buddywilliams@gmail.com>
2017-06-01 18:33:19 -0400

    added jakefile

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ echo 'Updating node deps...'
 yarn install
 
 echo 'Building Bliss...'
-node_modules/.bin/jake build
+node_modules/.bin/jake -f jakefile.js build
 
 echo 'Starting express web server...'
 forever start -a -p . -l ./logs/forever.log -o ./logs/bliss-out.log -e ./logs/bliss-err.log node_modules/.bin/jake -f jakefile.js server
