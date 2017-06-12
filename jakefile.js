141b5a12c373e0345bd9de1b9f7af7fcbcac6402
Buddy Williams <buddywilliams@gmail.com>
2017-06-12 08:09:24 -0400

    fixed language

--- jakefile.js
+++ jakefile.js
@@ -128,7 +128,7 @@ task('build', function(){
 desc('Updates Bliss after you change it in the UI (you need to build bliss first)');
 task('update-bliss', function() {
   var t = jake.Task['util:copy-files'];
-  t.execute.apply(t, ['json',`${config.bliss_build}`, `${config.bliss_src}`]);
+  t.execute.apply(t, ['json',`${config.bliss_build}/projects`, `${config.bliss_src}/projects`]);
 
   var t = jake.Task['build'];
   t.execute.apply(t);
