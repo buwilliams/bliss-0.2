434c42eb08db961ccb1015b4da64fa3c18f75a0e
Buddy Williams <buddywilliams@gmail.com>
2017-07-27 08:56:03 -0400

    designer middleware now working

--- src/server/designer-middleware.js
+++ src/server/designer-middleware.js
@@ -4,9 +4,12 @@ const session = require('./session.js');
 const ws = require('../compilers/core/workspace.js');
 
 module.exports = function(req, res, next) {
-  console.log('Designer url:', req.url, 'Token:', req.params.token)
+  console.log('Designer url:', req.url, 'Token:', req.params.token, 'Params:', req.params)
 
-  var validToken = tokens.verifyToken(req.params.token, env.secret_key);
+  var token = req.params.token;
+  var filePath = req.params['0'];
+
+  var validToken = tokens.verifyToken(token, env.secret_key);
   if(!validToken) {
     res.status(401).send('Invalid token');
     return;
@@ -15,5 +18,5 @@ module.exports = function(req, res, next) {
   var parts = tokens.getTokenParts(req.params.token);
   var newSession = Object.assign({}, session);
   newSession.user.username = parts[0];
-  res.sendFile(req.url, {root: ws.workspace(env, newSession)});
+  res.sendFile(filePath, { root: ws.workspace(env, newSession) });
 }
