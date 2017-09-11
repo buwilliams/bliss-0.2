7c8ad713fe78e8e037911e45e49c41c0bed519b5
Buddy Williams <buddywilliams@gmail.com>
2017-09-11 15:41:11 -0400

    workspace selection, oh my

--- src/workspaces/5d06f306c22c07a5/bliss/js/designer.js
+++ src/workspaces/5d06f306c22c07a5/bliss/js/designer.js
@@ -332,6 +332,14 @@ var blissUiV = (function() {
         dataType: 'json'
       });
     }
+    app.js['afterAuth'] = function(scope, attributes) {
+      app.js.log('after auth invoked')
+
+      app.dispatch({
+        path: '/workspaces',
+        action: 'fetch'
+      })
+    }
     app.methods["242"] = {};
     app.methods["242"]['shouldShow'] = function() {
       return (app.state.firebase.user) ? false : true;
@@ -341,12 +349,35 @@ var blissUiV = (function() {
       return (app.state.workspaces.active === true)
     }
     app.methods["252"] = {};
-    app.methods["252"]['workspaceList'] = function(scope, attributes) {
+    app.methods["252"]['repeater'] = function(scope, attributes) {
       return app.state.workspaces.list;
     };
 
     app.methods["252"]['getText'] = function(scope, attributes) {
-      return scope.workspaceList[scope.workspaceList_index];
+      var item = scope.repeater[scope.repeater_index]
+      return item.name
+    };
+
+    app.methods["252"]['handleClick'] = function(scope, attributes) {
+      var item = scope.repeater[scope.repeater_index]
+
+      return function(e) {
+        app.dispatch({
+          path: '/settings',
+          action: 'set',
+          key: 'workspace',
+          value: item.name
+        })
+
+        app.dispatch({
+          path: '/workspaces',
+          action: 'set',
+          key: 'active',
+          value: false
+        })
+
+        app.js.getProjects()
+      }
     };
     app.methods["243"] = {};
     app.methods["243"]['shouldShow'] = function() {
@@ -1158,8 +1189,11 @@ var blissUiV = (function() {
               path: '/firebase',
               action: 'setToken',
               user_token: idToken
-            });
-          });
+            })
+
+            app.js.afterAuth()
+
+          })
         } else {
           // Clear user state
           app.dispatch({
@@ -1230,14 +1264,43 @@ var blissUiV = (function() {
     app.schema['/workspaces'] = {};
     app.schema['/workspaces']['init'] = function(data, args) {
       var newData = {
-        active: false,
+        active: true,
         list: []
       }
       return newData
     }
-    app.schema['/workspaces']['add_workspace'] = function(data, args) {
+    app.schema['/workspaces']['add'] = function(data, args) {
       var newData = Object.assign({}, data)
       newData.list.push(args.item)
+      return newData;
+    }
+    app.schema['/workspaces']['fetch'] = function(data, args) {
+      $.ajax({
+        type: 'GET',
+        url: '/workspace/list',
+        data: data,
+        success: function(data) {
+          app.dispatch({
+            path: '/workspaces',
+            action: 'addAll',
+            list: data.workspaces
+          })
+        },
+        contentType: "application/json",
+        dataType: 'json'
+      });
+
+      return data;
+    }
+    app.schema['/workspaces']['addAll'] = function(data, args) {
+      var newData = Object.assign({}, data)
+      newData.list = args.list
+      return newData;
+    }
+    app.schema['/workspaces']['set'] = function(data, args) {
+      var newData = Object.assign({}, data);
+      if (newData.hasOwnProperty(args.key))
+        newData[args.key] = args.value;
       return newData;
     }
     if (app.schema['/workspaces']['init']) {
@@ -1545,20 +1608,19 @@ var blissUiV = (function() {
                 }),
                 (function(scope) {
                   var out = [];
-                  var list = scope['workspaceList'] = app.methods['252']['workspaceList'](scope);
+                  var list = scope['repeater'] = app.methods['252']['repeater'](scope);
                   for (var i = 0; i < list.length; i++) {
-                    scope['workspaceList_index'] = i;
-                    out.push(React.createElement('div', app.mergeAttributes('252', scope, {}, {
+                    scope['repeater_index'] = i;
+                    out.push(React.createElement('div', app.mergeAttributes('252', scope, {
+                      "onClick": "handleClick"
+                    }, {
+                      "className": "workspaces",
                       "id": "listOfWorkspaces_252",
                       "key": app.getKey('id', '252', i)
                     }), app.methods['252']['getText'](scope)));
                   }
                   return out;
-                })(scope),
-                React.createElement('div', app.mergeAttributes('256', scope, {}, {
-                  "id": "new_256",
-                  "key": app.getKey('id', '256')
-                }), 'hey there')));
+                })(scope)));
             }
             return out;
           })(scope),
