7ae1ac3ee4fa9e843fa3e66d5577399f9af8efea
Buddy Williams <buddywilliams@gmail.com>
2017-09-12 15:16:52 -0400

    changed start and restart scripts to use systemd

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo 'Stopping express web server...'
-sudo forever stopall
+#sudo forever stopall
+sudo systemctl stop bliss
 
 echo 'Starting express web server...'
-sudo forever start -a -p . -l ./logs/forever.log -o ./logs/bliss-out.log -e ./logs/bliss-err.log node_modules/.bin/jake -f jakefile.js server
+#sudo forever start -a -p . -l ./logs/forever.log -o ./logs/bliss-out.log -e ./logs/bliss-err.log node_modules/.bin/jake -f jakefile.js server
+sudo systemctl start bliss
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo 'Stopping express web server...'
-sudo forever stopall
+sudo systemctl stop bliss
+#sudo forever stopall
 
 echo 'Copying env file...'
 cp -f ./scripts/prod.env ./.env
@@ -21,4 +22,5 @@ echo 'Building Bliss...'
 node_modules/.bin/jake -f jakefile.js build
 
 echo 'Starting express web server...'
-sudo forever start -a -p . -l ./logs/forever.log -o ./logs/bliss-out.log -e ./logs/bliss-err.log node_modules/.bin/jake -f jakefile.js server
+#sudo forever start -a -p . -l ./logs/forever.log -o ./logs/bliss-out.log -e ./logs/bliss-err.log node_modules/.bin/jake -f jakefile.js server
+sudo systemctl start bliss
