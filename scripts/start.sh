cd479b1ce5bb68c36ffd81c8cc93999bd8f907e7
Buddy Williams <buddywilliams@gmail.com>
2017-09-14 10:05:49 -0400

    run all commands as sudo

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,18 +7,18 @@ echo 'Copying env file...'
 cp -f ./scripts/prod.env ./.env
 
 echo 'Creating tls and symlinks for https...'
-mkdir -p tls
-ln -sfT /etc/letsencrypt/live/blissui.com/fullchain.pem ./tls/cert.pem
-ln -sfT /etc/letsencrypt/live/blissui.com/privkey.pem ./tls/key.pem
+sudo mkdir -p tls
+sudo ln -sfT /etc/letsencrypt/live/blissui.com/fullchain.pem ./tls/cert.pem
+sudo ln -sfT /etc/letsencrypt/live/blissui.com/privkey.pem ./tls/key.pem
 
 echo 'Creating symlink for firebase...'
-ln -sfT ~/.ssh/blissui-firebase.json ./blissui-firebase.json
+sudo ln -sfT ~/.ssh/blissui-firebase.json ./blissui-firebase.json
 
 echo 'Updating node deps...'
-yarn install
+sudo yarn install
 
 echo 'Building Bliss...'
-node_modules/.bin/jake -f jakefile.js build
+sudo node_modules/.bin/jake -f jakefile.js build
 
 echo 'Starting express web server...'
 sudo systemctl start bliss
