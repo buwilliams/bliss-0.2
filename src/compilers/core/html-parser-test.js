e54af84ac0b572ded8ef6c870bfe5d309080a80b
Buddy Williams <buddywilliams@gmail.com>
2018-03-16 09:26:55 -0400

    checkpoint: working on appending component to projectJson

--- src/compilers/core/html-parser-test.js
+++ src/compilers/core/html-parser-test.js
@@ -5,10 +5,10 @@ const projectJson = require('./project-json.js');
 describe('htmlParser', function() {
   describe('parseFragment()', function() {
     it('should parse simple hello, world div', function() {
-      var html = "<div>hello, world</div>";
+      var html = `<div id="foo">hello, world</div>`;
       var frag = htmlParser.parseFragment(html)
 
-      console.log(frag);
+      console.log(frag.childNodes[0].attrs);
 
       expect(frag.childNodes[0].tagName).to.equal("div");
       expect(frag.childNodes[0].childNodes[0].value)
--- src/compilers/core/html-parser.js
+++ src/compilers/core/html-parser.js
@@ -11,14 +11,42 @@ module.exports = {
     return parsed;
   },
 
+  appendComponent: function(htmlRef, projectJson, parentId, prevId) {
+    var component = {
+      "id": null,
+      "name": htmlRef.tagName,
+      "element": htmlRef.tagName,
+      "text": null,
+      "attributes": [],
+      "dynamicAttributes": [],
+      "css": [],
+      "js": [],
+      "next": null,
+      "previous": prevId,
+      "child": null,
+      "parent": parentId
+    };
+
+    htmlRef.attrs.forEach(function(attr) {
+      component.attributes.push(attr);
+    });
+
+    component.id = projectJson.nextId++;
+    projectJson.components[component.id] = component;
+
+    return component.id;
+  },
+
   toProject: function(htmlString, projectJson, appendId) {
     var frag = this.parseFragment(htmlString);
     return this._toProject(frag.childNodes[0], projectJson, appendId);
   },
 
-  _toProject: function(htmlRef, projectJson, appendId) {
+  _toProject: function(htmlRef, projectJson, parentId) {
     // create component
+
     // set attributes
+    // loop through childNodes
     // set parent, child, next, previous
     return projectJson;
   }
