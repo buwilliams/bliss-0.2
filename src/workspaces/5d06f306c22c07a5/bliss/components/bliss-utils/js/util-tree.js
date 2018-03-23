ae2633349304e752fb53d711609fa0723c941ff2
Buddy Williams <buddywilliams@gmail.com>
2018-03-23 07:40:21 -0400

    Build updated UtilTree

--- src/workspaces/5d06f306c22c07a5/bliss/components/bliss-utils/js/util-tree.js
+++ src/workspaces/5d06f306c22c07a5/bliss/components/bliss-utils/js/util-tree.js
@@ -229,7 +229,7 @@ var UtilTree = {
   },
 
   moveComponent: function(proj, fromId, toId, shouldBeChild) {
-    var hasChild = this.hasChild(proj, fromId, toId);
+    var hasChild = this.hasChildDescendant(proj, fromId, toId);
     if(fromId === toId || hasChild) return proj;
 
     this.removeFromTree(proj, fromId);
