002c393e673d275e9d8c49349b91165b725e4857
Buddy Williams <buddywilliams@gmail.com>
2021-01-11 22:08:53 -0500

    first working version, saving project json files still isn't working

--- src/server/designer-middleware.js
+++ src/server/designer-middleware.js
@@ -12,24 +12,25 @@ module.exports = function(req, res, next) {
     'Params:', req.params)
   */
 
-  var token = req.params.token;
+  //var token = req.params.token;
   var workspace = req.params.workspace;
   var filePath = req.params['0'];
 
-  var validToken = tokens.verifyToken(token, env.secret_key);
+  /*var validToken = tokens.verifyToken(token, env.secret_key);
   if(!validToken) {
     res.status(401).send('Invalid token');
     return;
-  }
+  }*/
 
   if(!workspace) {
     res.status(400).send('Missing workspace path in url');
     return;
   }
 
-  var parts = tokens.getTokenParts(req.params.token);
+  //var parts = tokens.getTokenParts(req.params.token);
   var newSession = Object.assign({}, session);
-  newSession.user.username = parts[0];
+  // newSession.user.username = parts[0];
+  newSession.user.username = env.bliss_user; // '5d06f306c22c07a5'
 
   // Initialize user and workspace
   var u = user(env, newSession).createUser()
